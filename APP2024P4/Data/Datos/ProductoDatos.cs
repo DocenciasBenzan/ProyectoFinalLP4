14423fccd438b3344a1c0d0343bf51b6e813915c
LENOVO <LENOVO@LAPTOP-573KV0C0>
2024-11-23 15:26:45 -0400

    agregando Dto de producto

--- APP2024P4/Data/Datos/ProductoDatos.cs
+++ APP2024P4/Data/Datos/ProductoDatos.cs
@@ -1,6 +1,6 @@
 ﻿namespace APP2024P4.Data.Datos
 {
-    public record ProductoDato(int Id, string Nombre, int CategoriaId, DateTime FechaL, string? Color, int Cantidad, int ModeloId, decimal Precio, string? Descripcion, string? Imagen )
+    public record ProductoDato(int Id, string Nombre, int CategoriaId, DateTime FechaL, string? Color, int Cantidad, int ModeloId, decimal Precio, string? Descripcion, string Imagen)
     {
         public ProductoRequest ToRequest()
         => new()
@@ -8,17 +8,31 @@
             Id = this.Id,
             Nombre = this.Nombre,
             CategoriaId = this.CategoriaId,
-            //FechaL = this.FechaL
+            FechaL = this.FechaL,
+            Color = this.Color,
+            Cantidad = this.Cantidad,
+            ModeloId = this.ModeloId,
+            Precio = this.Precio,
+            Descripcion = this.Descripcion,
+            Imagen = this.Imagen
 
-            
+
+
         };
     };
     public class ProductoRequest
     {
         public int Id { get; set; } = 0;
         public string Nombre { get; set; } = "";
-        public string? Descripcion { get; set; } = null;
         public int? CategoriaId { get; set; }
+        public DateTime FechaL { get; set; }
+        public string? Color { get; set; }
+        public int Cantidad { get; set; }
+        public int ModeloId { get; set; }
         public decimal Precio { get; set; }
+        public string? Descripcion { get; set; } = null;
+        public string Imagen { get; set; } = null!;
+
+
     }
 }
