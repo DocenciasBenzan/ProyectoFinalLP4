dcf740445762a99695c8f94b34a72520326e794c
deens <deens@FRANLABPC>
2024-11-28 19:26:47 -0400

    arriba y en funcion

--- APP2024P4/Program.cs
+++ APP2024P4/Program.cs
@@ -26,13 +26,13 @@ builder.Services.AddAuthentication(options =>
     .AddIdentityCookies();
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
+builder.Services.AddDbContext<DatabaseApp>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddScoped<IDatabaseApp, DatabaseApp>();
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddIdentityCore<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddEntityFrameworkStores<DatabaseApp>()
     .AddSignInManager()
     .AddDefaultTokenProviders();
 
---
+++ APP2024P4/services/GroceryApi.cs
@@ -0,0 +1,14 @@
+﻿namespace APP2024P4.services;
+
+using System.Net.Http;
+using System.Net.Http.Json;
+
+public class GroceryApi
+{
+    private readonly HttpClient _httpClient;
+    public GroceryApi(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+}
