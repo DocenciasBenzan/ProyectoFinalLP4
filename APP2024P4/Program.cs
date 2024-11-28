b271c8560663d8ec95669541a315f01d6667992d
deens <deens@FRANLABPC>
2024-11-28 18:36:01 -0400

    new migration

--- APP2024P4/Program.cs
+++ APP2024P4/Program.cs
@@ -29,8 +29,8 @@ builder.Services.AddAuthentication(options =>
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<DatabaseApp>(options =>
     options.UseSqlServer(connectionString));
-builder.Services.AddScoped<IDatabaseApp, DatabaseApp>();
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
 
 builder.Services.AddIdentityCore<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<DatabaseApp>()
