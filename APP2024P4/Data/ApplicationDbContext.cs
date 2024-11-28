07beef3b145d96e5b4f733767909bd7d05a3450c
DIEGO STEWART BEATO FIGUEROA <2021-1275@uteco.edu.do>
2024-11-28 19:44:20 -0400

    Avance en el Front.

--- APP2024P4/Data/ApplicationDbContext.cs
+++ APP2024P4/Data/ApplicationDbContext.cs
@@ -1,9 +1,23 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using APP2024P4.Data.Entities;
 
 namespace APP2024P4.Data
 {
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : 
+        IdentityDbContext<ApplicationUser>(options), IApplicationDbContext
+    {
+        public class DbSet<Producto> productos { get; set; }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+        return base.SaveChangesAsync(cancellationToken);
+        }
+    }
+
+    /*
     public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
     {
     }
+    */
 }
---
+++ APP2024P4/Data/IApplicacionDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using APP2024P4.Data.Entities;
+
+namespace APP2024P4.Data
+{
+    public class IApplicacionDbContext
+    {
+        DbSet<Producto> Productos { get; set; }
+
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+    }
+}
