1a1f4c5ff7e0eb674fd5f157bd5aebcd5d30e916
deens <deens@FRANLABPC>
2024-12-04 22:55:57 -0400

    probando Migrations

--- APP2024P4/Data/Migrations/20241205025303_Productos.Designer.cs
+++ APP2024P4/Data/Migrations/20241205025303_Productos.Designer.cs
@@ -12,8 +12,8 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace APP2024P4.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20241130175652_AddImgToProducto")]
-    partial class AddImgToProducto
+    [Migration("20241205025303_Productos")]
+    partial class Productos
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
